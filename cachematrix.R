ce6e9423d77f17eab7933edcbbcc704735c6c3f7
msantos68 <msantos6@travelers.com>
2015-02-14 14:58:44 -0500

    Changes to cachematrix.R include edits to calculate and cache the Inverse of a Matrix using the Solve function in R. Edits done on 2/14/2015 by Michelle Santos

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Michelle Santos 2/9/2015
+## Calculating the matrix inverse can be computational intensive.
+## Functions below will calculate and cache the matrix inverse.
+## If the matrix inverse is required it will determine whether or not it 
+## is already cached before recomputing.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function will cache the matrix inverse into the 
+## object m
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setMI <- function(solve) m <<- solve
+  getMI <- function() m
+  list(set = set, 
+       get = get,
+       setMI = setMI,
+       getMI = getMI)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function will look to see if the matrix inverse is cached
+##  If not it will calculate the matrix inverse and cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getMI()
+  if(!is.null(m)) {
+    message("getting cached matrix inverse")
+    ## Return a matrix that is the inverse of 'x'
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setMI(m)
+  ## Return a matrix that is the inverse of 'x'
+  m
+  
+        
 }
